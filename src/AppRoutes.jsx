1b530fe094011f3ab4dbbe175b8d7358100d865d
Taguhi <taguhikarakash@gmail.com>
2022-05-12 20:35:11 +0200

    dashboard css

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -11,7 +11,7 @@ export const LogInContext = createContext()
 
 const AppRoutes = () => {
 
-  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const [isLoggedIn, setisLoggedIn] = useState(true);
 
 
 
