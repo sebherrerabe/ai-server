11a6e97e4eb659e3ab959292c1d857a8ec90617b
Taguhi <taguhikarakash@gmail.com>
2022-05-13 14:42:30 +0200

    styling

--- src/pages/Dashboard/components/Past Trainings/Trainings.jsx
+++ src/pages/Dashboard/components/Past Trainings/Trainings.jsx
@@ -3,46 +3,48 @@ import { ThemeContext } from "../../../../Layout/Layout";
 import "./Trainings.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faLink} from '@fortawesome/free-solid-svg-icons';
+import { faDownload} from '@fortawesome/free-solid-svg-icons';
+import { faCheck} from '@fortawesome/free-solid-svg-icons';
 
 const Trainings = () => {
   const themeColors = useContext(ThemeContext);
   return (
     <div className="component-container">
-      <div className="training-number">You have 3 training in queue</div>
+      <div className={"training-number  " + themeColors.textTertiaryColor}>You have 3 trainings in queue</div>
       <div className="training-table">
         <table>
           <thead>
             <tr>
-              <th>Container details</th>
-              <th>Status</th>
-              <th>Artifacts</th>
-              <th>Finish date</th>
+              <th className={"container-details  " + themeColors.textTertiaryColor}>Container details</th>
+              <th className={"status  " + themeColors.textTertiaryColor}>Status</th>
+              <th className={"artifacts  " + themeColors.textTertiaryColor}>Artifacts</th>
+              <th className={"finish-date  " + themeColors.textTertiaryColor}>Finish date</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Details</td>
-              <td>Done</td>
-              <td>Download</td>
-              <td>Date</td>
+              <td className={"details  "+ themeColors.textTertiaryColor}><FontAwesomeIcon icon={faLink} className={"icons  " + themeColors.textTertiaryColor}/>Details</td>
+              <td className={"done  " + themeColors.textTertiaryColor}><FontAwesomeIcon icon={faCheck} className={"icons  " +themeColors.textTertiaryColor}/>Done</td>
+              <td className={"download  " + themeColors.textTertiaryColor}><FontAwesomeIcon icon={faDownload} className={"icons  " + themeColors.textTertiaryColor} />Download</td>
+              <td className={"date  " +  themeColors.textTertiaryColor}>Date</td>
             </tr>
             <tr>
-              <td>Details</td>
-              <td>Done</td>
-              <td>Download</td>
-              <td>Date</td>
+            <td className={"details  "+ themeColors.textTertiaryColor}><FontAwesomeIcon icon={faLink} className={"icons  " + themeColors.textTertiaryColor}/>Details</td>
+            <td className={"done  " + themeColors.textTertiaryColor}><FontAwesomeIcon icon={faCheck} className={"icons  " +themeColors.textTertiaryColor}/>Done</td>
+            <td className={"download  " + themeColors.textTertiaryColor}><FontAwesomeIcon icon={faDownload} className={"icons  " + themeColors.textTertiaryColor} />Download</td>
+            <td className={"date  " +  themeColors.textTertiaryColor}>Date</td>
             </tr>
             <tr>
-              <td>Details</td>
-              <td>Done</td>
-              <td>Download</td>
-              <td>Date</td>
+            <td className={"details  "+ themeColors.textTertiaryColor}><FontAwesomeIcon icon={faLink} className={"icons  " + themeColors.textTertiaryColor}/>Details</td>
+            <td className={"done  " + themeColors.textTertiaryColor}><FontAwesomeIcon icon={faCheck} className={"icons  " +themeColors.textTertiaryColor}/>Done</td>
+            <td className={"download  " + themeColors.textTertiaryColor}><FontAwesomeIcon icon={faDownload} className={"icons  " + themeColors.textTertiaryColor} />Download</td>
+            <td className={"date  " +  themeColors.textTertiaryColor}>Date</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div className="training-refreshing-msg">Refreshing every 30 seconds</div>
+      <div className={"training-refreshing-msg  " + themeColors.textTertiaryColor}>Refreshing every 30 seconds</div>
     </div>
   );
 };
--- src/pages/Dashboard/components/Queue/Queue.jsx
+++ src/pages/Dashboard/components/Queue/Queue.jsx
@@ -9,10 +9,31 @@ const Queue = () => {
   const themeColors = useContext(ThemeContext);
   return (
     <div className="component-container">
-      <div className="display-queue">
-        <div className="queue-number">You have 0 training in queue</div>
-        <div className="refreshing-msg">Refreshing every 30 seconds</div>
+      <div className={"training-number  " + themeColors.textTertiaryColor}>You have 0 training in queue</div>
+      <div className="training-table">
+        <table>
+          <thead>
+            <tr>
+              <th className={"container-details  " + themeColors.textTertiaryColor}>The docker image name</th>
+              <th className={"status  " + themeColors.textTertiaryColor}>The position of the training in the queue.</th>
+              <th className={"artifacts  " + themeColors.textTertiaryColor}>The docker volume.</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className={"details  "+ themeColors.textTertiaryColor}></td>
+              <td className={"done  " + themeColors.textTertiaryColor}></td>
+              <td className={"download  " + themeColors.textTertiaryColor}></td>
+            </tr>
+            <tr>
+              <td className={"details  "+ themeColors.textTertiaryColor}></td>
+              <td className={"done  " + themeColors.textTertiaryColor}></td>
+              <td className={"download  " + themeColors.textTertiaryColor}></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+      <div className={"training-refreshing-msg  " + themeColors.textTertiaryColor}>Refreshing every 30 seconds</div>
     </div>
   );
 };
