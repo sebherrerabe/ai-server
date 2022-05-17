c9f81f9e818bbc4e26f3bacec788a42db110f50a
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-05-17 09:29:37 +0200

    queue table width defined

--- src/pages/Dashboard/components/Queue/Queue.jsx
+++ src/pages/Dashboard/components/Queue/Queue.jsx
@@ -96,7 +96,7 @@ const Queue = () => {
           <div className={"training-number  " + themeColors.textTertiaryColor}>You have {queue.length} {queue.length === 1 ? "training" : "trainings"} in queue</div>
           <div className="training-table">
             {queue.length !== 0 ?
-              <table>
+              <table id="queue">
                 <thead>
                   <tr className={themeColors.textTertiaryColor}>
                     <th>Position</th>
