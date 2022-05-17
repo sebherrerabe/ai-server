6a30d119f76ddfb029d19455cdc571599fa9288b
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-05-17 12:21:42 +0200

    icons centered in table cells

--- src/pages/Dashboard/components/Past Trainings/components/Row.jsx
+++ src/pages/Dashboard/components/Past Trainings/components/Row.jsx
@@ -3,29 +3,31 @@ import { ThemeContext } from "../../../../../Layout/Layout";
 
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink} from '@fortawesome/free-solid-svg-icons';
-import { faDownload} from '@fortawesome/free-solid-svg-icons';
-import { faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-const Row = ({containerId, status, artifactsUrl, finishDate}) => {
-
+const Row = ({ containerId, status, artifactsUrl, finishDate }) => {
     const themeColors = useContext(ThemeContext);
-    return ( 
+    return (
         <tr>
-        <td className={"container-details  " + themeColors.textTertiaryColor}>
-            <a className={themeColors.textTertiaryColor} href={`http://portainer.ai-server.becode.org/#!/2/docker/containers/${containerId}`}target="_blank">
-            <div className="icon">
-        <FontAwesomeIcon icon={faLink} />
-         </div>
-         </a></td>
-        <td className={"status  " + themeColors.textTertiaryColor}>{status === "DONE" ? <FontAwesomeIcon icon={faCheck} />: "in progress"}<div className="icon">
-        </div></td>
-        <td className={"artifacts  " + themeColors.textTertiaryColor}><div className="icon">
-        <a className={themeColors.textTertiaryColor} href={artifactsUrl} target="_blank" ><FontAwesomeIcon icon={faDownload} /></a> </div></td>
-        <td className={"finish-date  " + themeColors.textTertiaryColor}>{finishDate}</td>
-      </tr>
-     );
-    }
+            <td className={"container-details  " + themeColors.textTertiaryColor}>
+                <div className="a-con">
+                    <a className={themeColors.textTertiaryColor} href={`http://portainer.ai-server.becode.org/#!/2/docker/containers/${containerId}`} target="_blank" rel="noreferrer">
+                        <div className="icon">
+                            <FontAwesomeIcon icon={faLink} />
+                        </div>
+                    </a>
+                </div></td>
+            <td className={"status  " + themeColors.textTertiaryColor}><div className="a-con"> <div className="icon"><FontAwesomeIcon icon={faCheck} /></div> </div></td>
+            <td className={"artifacts  " + themeColors.textTertiaryColor}>
+                <div className="a-con">
+                    <a className={themeColors.textTertiaryColor} href={artifactsUrl} target="_blank" rel="noreferrer" ><div className="icon"><FontAwesomeIcon icon={faDownload} /></div></a>
+                </div></td>
+            <td className={"finish-date  " + themeColors.textTertiaryColor}>{finishDate}</td>
+        </tr>
+    );
+}
 
 
 export default Row;
